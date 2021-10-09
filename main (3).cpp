b33733e4a5061efe3e56248ea08d977d671d53f9
IvanEgorov1 <80792526+IvanEgorov1@users.noreply.github.com>
2021-10-09 18:33:10 +0300

    Add files via upload

---
+++ main (3).cpp
@@ -0,0 +1,25 @@
+﻿#include "Program.h"
+ 
+int main () {
+    Дата date_1 (2020, 12, 3);
+    Дата date_2 (2020, 12, 4);
+ 
+    cout << date_2.GetA () << endl;
+    cout << date_2.GetB () << endl;
+    cout << date_2.GetC () << endl;
+ 
+    date_1.Print ();
+    date_2.Print ();
+ 
+    Дата date_3;
+    date_3.SetA (1980);
+    date_3.SetB (10);
+    date_3.SetC (16);
+ 
+    cout << (date_3 <date_2) << endl;
+    cout << (date_3 == date_1) << endl;
+    cout << (date_1> date_2) << endl;
+    cout << (date_2! = date_3) << endl;
+ 
+    возврат 0;
+}
